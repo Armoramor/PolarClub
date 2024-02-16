eb900633eb3f875f49b7d0d1cfee6e915dd2aa96
Marine Sance <marine.sance05@gmail.com>
2024-02-16 17:49:11 +0100

    modification css / html / js

--- js/script.js
+++ js/script.js
@@ -7,18 +7,18 @@
 // // let sect = document.getElementByid('sect');
 
 window.addEventListener("scroll", function () {
-    let nuage = document.getElementById('troisiemeplan');
+    // let nuage = document.getElementById('troisiemeplan');
     let coline = document.getElementById('deuxiemeplan');
-    let sapin = document.getElementById('premierplan');
-    let title = document.getElementById('title');
-    let btn = document.getElementById('btn');
-    let sect = document.getElementById('sect');
+    // let sapin = document.getElementById('premierplan');
+    // let title = document.getElementById('titleid');
+    // let btn = document.getElementById('btn');
+    // let sect = document.getElementById('sect');
     let value = window.scrollY;
     let lune = document.getElementById('lune');
-    // nuage.style.top = value + 'px';
+
     lune.style.top = value * 0.80 + "px";
     coline.style.top = value * 0.30 + 'px';
-    // sapin.style.top = value + 'px';
+
 
 
 })

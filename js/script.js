6d714632856b611091b6a194ef227e8733eef5f4
Marine Sance <marine.sance05@gmail.com>
2024-02-16 13:27:46 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+// let nuage = document.getElementByid('troisiemeplan');
+// let coline = document.getElementByid('deuxiemeplan');
+// let sapin = document.getElementByid('premierplan');
+// let title = document.getElementByid('title');
+// let btn = document.getElementByid('btn');
+// // let sect = document.getElementByid('sect');
+
+window.addEventListener("scroll", function () {
+    let nuage = document.getElementById('troisiemeplan');
+    let coline = document.getElementById('deuxiemeplan');
+    let sapin = document.getElementById('premierplan');
+    let title = document.getElementById('title');
+    let btn = document.getElementById('btn');
+    let sect = document.getElementById('sect');
+    let value = window.scrollY;
+    let lune = document.getElementById('lune');
+    // nuage.style.top = value + 'px';
+    lune.style.top = value * 0.80 + "px";
+    coline.style.top = value * 0.30 + 'px';
+    // sapin.style.top = value + 'px';
+
+
+})
